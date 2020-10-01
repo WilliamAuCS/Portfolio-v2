ef7ac65c03b295db568dff17c21e8e9852c02b35
William Au <WilliamAuCS@Gmail.com>
2020-09-30 22:48:14 -0700

    Centered cards

--- williamaucs-com/src/app/modules/sandbox/sandbox.component.ts
+++ williamaucs-com/src/app/modules/sandbox/sandbox.component.ts
@@ -22,7 +22,7 @@ interface regristration_array {
 @Component({
   selector: 'app-sandbox',
   templateUrl: './sandbox.component.html',
-  styleUrls: ['./sandbox.component.css']
+  styleUrls: ['./sandbox.component.css'], 
 })
 export class SandboxComponent implements OnInit {
 
