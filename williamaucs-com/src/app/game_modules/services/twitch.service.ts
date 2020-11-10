582dfb567bd62a8b6c1f286fba3857a9bce8eba6
William Au <WilliamAuCS@Gmail.com>
2020-11-09 23:45:53 -0800

    Updated url

--- williamaucs-com/src/app/game_modules/services/twitch.service.ts
+++ williamaucs-com/src/app/game_modules/services/twitch.service.ts
@@ -6,8 +6,8 @@ import { Injectable } from '@angular/core';
 })
 export class TwitchService {
 
-  // private get_stream_info: string = 'https://server.makosusa.com:8080/api/twitch/getUser?';
-  private get_stream_info: string = 'http://localhost:8080/api/twitch/getUser/';
+  private get_stream_info: string = 'https://server.makosusa.com:8080/api/twitch/getUser/';
+  // private get_stream_info: string = 'http://localhost:8080/api/twitch/getUser/';
 
   constructor(private _http: HttpClient) { }
 
